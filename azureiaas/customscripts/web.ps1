b3d6a5f0fca9b2f21a61c7ba1c7f271126f060f8
Uday Pandya <udpandya@microsoft.com>
2017-08-21 17:57:58 +0100

    v2 rebase

---
+++ azureiaas/customscripts/web.ps1
@@ -0,0 +1,9 @@
+﻿Write-Host "Hello World!"
+
+Set-ExecutionPolicy AllSigned; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+choco install git.install -y
+choco install dotnetcore -y
+choco install dotnetcoresdk -y
