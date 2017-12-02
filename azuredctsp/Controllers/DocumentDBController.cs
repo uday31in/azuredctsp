1d0517431d00b4fbfb1872551b86313f2a4889fa
Uday Pandya <udpandya@microsoft.com>
2017-12-02 13:50:46 +0000

    Adding App Insight Telemetry

--- azuredctsp/Controllers/DocumentDBController.cs
+++ azuredctsp/Controllers/DocumentDBController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,7 @@ using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Microsoft.ApplicationInsights.DataContracts;
 
 
 namespace azuredctsp.Controllers
@@ -28,7 +30,11 @@ namespace azuredctsp.Controllers
         private const int MinThreadPoolSize = 100;
         
         static AzureDocumentDBHelper dbhelper;
+
+        private TelemetryClient telemetry = new TelemetryClient();
+
         
+
 
         //public DocumentDBController(IOptions<DocumentDBConfig> config, IHostingEnvironment hostingEnvironment, IHubContext<Chat> hub)
         public DocumentDBController(IOptions<DocumentDBConfig> config, IHostingEnvironment hostingEnvironment)
@@ -74,11 +80,18 @@ namespace azuredctsp.Controllers
         [HttpGet]
         public async Task<ActionResult> GetTimeSeries(string id, CancellationToken cancellationToken)
         {
+            
             List<DocumentDBStatus> timeseries = new List<DocumentDBStatus>();
             timeseries =  dbhelper.GetTimeSeries(id);
             
             if (timeseries!=null)
             {
+                var metric = new MetricTelemetry();
+                metric.Name = "RUs";
+                metric.Sum = timeseries[0].rus;
+                telemetry.TrackMetric(metric);
+
+                telemetry.TrackMetric(metric);
                 return new JsonStringResult(JsonConvert.SerializeObject(timeseries, Formatting.Indented));
             }
             else
